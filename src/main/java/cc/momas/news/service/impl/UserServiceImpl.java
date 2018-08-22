32a23f9df8be2673588662543e2b4b01c0317e59
Sod-Momas <sothereer@gmail.com>
2018-08-23 00:18:39 +0800

    change md5 implement

--- src/main/java/cc/momas/news/service/impl/UserServiceImpl.java
+++ src/main/java/cc/momas/news/service/impl/UserServiceImpl.java
@@ -2,8 +2,9 @@ package cc.momas.news.service.impl;
 
 import javax.servlet.ServletException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import cc.momas.news.common.BeanFactory;
-import cc.momas.news.common.DigestUtil;
 import cc.momas.news.dao.UserDao;
 import cc.momas.news.entity.User;
 import cc.momas.news.exception.BizException;
@@ -12,10 +13,10 @@ import cc.momas.news.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	private static final UserDao userDao = (UserDao) BeanFactory.getBean(BeanFactory.DAO_USER);
-	
+
 	@Override
 	public User login(Integer userId, String password) throws ServletException {
-		password = DigestUtil.md5Hex(password); // md5加密一下
+		password = DigestUtils.md5Hex(password.getBytes()); // md5加密一下
 		User user = userDao.selectByIdAndPwd(userId, password);
 		if (user == null) {
 			throw new BizException("用户不存在或密码错误");
